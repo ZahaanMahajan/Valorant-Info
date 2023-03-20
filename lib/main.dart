bd382c43a0579f4006e9ad77a2fdbcc04f68788e
Zahaan <zahaanjavaid032@gmail.com>
2023-03-20 11:49:39 +0530

    example screenshots added

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.system,
       home: const SplashScreen(),
     );
   }
